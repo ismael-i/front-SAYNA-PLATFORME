77b70ee60018494e48481a3d2765005b89a95382
ony yunis <yunis.thn@gmail.com>
2020-07-31 12:01:52 +0300

    plateforme52

--- js/main-slider1.js
+++ js/main-slider1.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function() {
+    /*-----------------
+        MAIN SLIDER
+    -----------------*/
+    $('.main-slider').bxSlider({
+        pagerCustom: '.pager-container',
+        controls: false,
+        easing: 'ease-in',
+        speed: 800,
+        auto: true,
+        pause: 6000
+    });
+});
